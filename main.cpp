cf31a1f439c8ce3e8491803860f954e307f623a7
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2012-08-29 18:25:09 +0200

    Removes old code.

--- main.cpp
+++ main.cpp
@@ -79,12 +79,15 @@ int main(int argc, char **argv)
 
 	boost::shared_ptr< TrainingSetVector > training_sets = generate_training_sets(training_classes);
 
+	/*
+	// To export the training set
 	for(int i = 0; i < training_classes->size(); ++i)
 	{
 		std::ostringstream output_file;
 		output_file << cli_parser.get_export_dir() << "/training_set_" << std::setfill('0') << std::setw(6) << i << ".data";
 		fann_save_train(training_sets->operator[](i).get(), output_file.str().c_str());
 	}
+	*/
 
 	boost::shared_ptr< NeuralNetworkVector > networks = train_neural_networks(training_sets);
 
@@ -170,71 +173,6 @@ int main(int argc, char **argv)
 		delete graph;
 	}
 
-	/*
-	   fann_shuffle_train_data(training_data);
-
-	   struct fann* ann = fann_create_standard(3, 8, 3, 1);
-	   fann_set_activation_function_hidden(ann, FANN_SIGMOID);
-	   fann_set_activation_function_output(ann, FANN_SIGMOID);
-	   fann_set_train_stop_function(ann, FANN_STOPFUNC_MSE);
-	   fann_set_learning_rate(ann, 0.1);
-
-	   fann_train_on_data(ann, training_data, 1000, 0, 0.0001);
-	   std::cout << fann_get_MSE(ann) << std::endl;
-
-
-
-	   ImageType::Pointer out = ImageType::New();
-	   out->SetRegions(requestedRegion);
-	   out->Allocate();
-
-	   ImageIterator in_it(rescaler->GetOutput(), rescaler->GetOutput()->GetLargestPossibleRegion());
-	   ImageIterator out_it(out, rescaler->GetOutput()->GetLargestPossibleRegion());
-
-	   fann_type *input = new fann_type[8];
-	   fann_type *output;
-
-	   while(!in_it.IsAtEnd())
-	   {
-	   windowIndex = in_it.GetIndex();
-	   windowIndex -= windowRadius;
-
-	   windowRegion.SetIndex(windowIndex);
-	   windowRegion.SetSize(windowSize);
-	   windowRegion.Crop(requestedRegion);
-
-	   featuresComputer->SetRegionOfInterest(windowRegion);
-	   featuresComputer->Update();
-
-	   raw_data.clear();
-
-	   input[0] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Energy );
-	   input[1] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Entropy ) ;
-	   input[2] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Correlation ) ;
-	   input[3] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::InverseDifferenceMoment ) ;
-	   input[4] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::Inertia ) ;
-	   input[5] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::ClusterShade ) ;
-	   input[6] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::ClusterProminence ) ;
-	   input[7] = featuresComputer->GetFeature( ScalarImageToLocalHaralickTextureFeaturesFilter::HaralickFeaturesComputer::HaralickCorrelation );
-
-	   for(unsigned int i = 0; i < 8; ++i)
-	   {
-	   input[i] = (input[i] - data_input_min[i]) / (data_input_max[i] - data_input_min[i]);
-	   }
-
-	   output = fann_run(ann, input);
-
-	   out_it.Set((unsigned int)(output[0] * 255));
-
-	   ++in_it;
-	   ++out_it;
-	   }
-
-	   WriterType::Pointer writer = WriterType::New();
-	   writer->SetInput(out);
-	   writer->SetFileName("out.bmp");
-	   writer->Update();
-	 */
 
 	return 0;
 }
